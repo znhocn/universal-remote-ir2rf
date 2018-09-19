7710e9a15e1160eb4f56d1cc73b16558db81e09f
ZnHoCn <me@hakr.xyz>
2018-09-20 07:58:50 +0800

    Update

---
+++ rf_to_ir_tiny/rf_to_ir_tiny.ino
@@ -0,0 +1,53 @@
+#include <IRremote.h>
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+IRsend irsend;
+IRrecv irrecv(3);
+decode_results results;
+
+void setup() {
+  //Serial.begin(9600);
+  mySwitch.enableReceive(0);  // RF Receiver on interrupt 0 => that is pin #2
+  mySwitch.enableTransmit(4);  // RF Transmitter is connected to Arduino pin #4
+  irrecv.enableIRIn();  // Start the receiver
+  // IR LED Transmitter is connected to Arduino pin #3
+  // IR VS1838B Receiver is connected to Arduino pin #1
+  
+  // Optional set pulse length.
+  // mySwitch.setPulseLength(320);
+  mySwitch.setPulseLength(170);
+  
+  // Optional set protocol (default is 1, will work for most outlets)
+  // mySwitch.setProtocol(2);
+  
+  // Optional set number of transmission repetitions.
+  // mySwitch.setRepeatTransmit(15);
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {    // Receiving IR signal to RF signal
+    //Serial.print("IR Received: ");
+    //Serial.println(results.value);
+    mySwitch.send(results.value, 24);
+    irrecv.resume();
+    delay(100);
+  } else {                          // Receiving RF signal to IR signal
+    if (mySwitch.available()) {
+      int value = mySwitch.getReceivedValue();
+      if (value != 0) {
+        //Serial.print("RF Received:  ");
+        //Serial.println(mySwitch.getReceivedValue());
+        irsend.sendNEC(mySwitch.getReceivedValue(), 28);
+        delay(90);
+      }
+      mySwitch.resetAvailable();
+      //softReset();
+    }
+  }
+}
+
+void softReset() {
+  asm volatile ("  jmp 0");
+}
+
